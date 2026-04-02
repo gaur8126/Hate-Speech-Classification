489c054e3d66079a6298443f19e0036bf01b9674
gaur8126 <gaurlokesh1211@gmail.com>
2026-04-02 19:49:39 +0530

    requirements.txt added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classifiaction",
+    version="0.0.0.1",
+    author="Lokesh",
+    packages=find_packages(),
+    install_requires=[]
+)
